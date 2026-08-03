501a66ffc6b866cf6e31828b883e0a3f0752cf5a
Sammy5code <najeemgbolahan4@gmail.com>
2026-08-03 01:27:45 +0000

    creating a chineseChef class

--- My-code/Chef.py
+++ My-code/Chef.py
@@ -7,4 +7,4 @@ class Chef:
         print("The chef makes a salad")  
 
     def make_special_dish(self):
-        print("The chef makes a special dish")  
+        print("The chef makes a bbq ribs")  
--- My-code/main.py
+++ My-code/main.py
@@ -1,1 +1,6 @@
+from Chef import Chef
 
+myChef = Chef()
+myChef.make_chicken()
+myChef.make_salad()
+myChef.make_special_dish()
