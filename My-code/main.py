7dbd7f534acb40999e5d609713289935dacb4bee
Sammy5code <najeemgbolahan4@gmail.com>
2026-08-03 01:04:26 +0000

    Creating a Student class

--- My-code/main.py
+++ My-code/main.py
@@ -1,26 +1,7 @@
-from Question import Question
+from Student import Student
 
-question_prompts = [
-    "What color are apples?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
-    "What color are Bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
-    "What color are strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
-]
-
-questions = [
-    Question(question_prompts[0], "a"),
-    Question(question_prompts[1], "c"),
-    Question(question_prompts[2], "b"),
-]
-
-def run_test(questions):
-    score = 0 
-    for question in questions:
-        answer = input(question.prompt)
-        if answer == question.answer:
-            score += 1
-    print("You got " + str(score) + "/" + str(len(questions)) + " Correct")
-
-run_test(questions)
+Student1 = Student("Oscar", "Accounting", 3.1)
+Student2 = Student("Phyllis", "Business", 3.8)
 
 
 
