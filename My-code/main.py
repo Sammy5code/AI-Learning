7ea5c25c240e1e9bcde1dded605724393f530b7e
Sammy5code <najeemgbolahan4@gmail.com>
2026-08-03 01:37:59 +0000

    Save commits

--- My-code/main.py
+++ My-code/main.py
@@ -1,6 +1,9 @@
 from Chef import Chef
+from ChineseChef import ChineseChef
 
 myChef = Chef()
 myChef.make_chicken()
-myChef.make_salad()
-myChef.make_special_dish()
+
+myChineseChef = ChineseChef()
+myChineseChef.make_chicken()
+
