a968947d301c7d691a7d10e23ee94ac3c59b9243
Sammy5code <najeemgbolahan4@gmail.com>
2026-09-08 22:44:50 +0100

    property

--- Python OOP/oop.py
+++ Python OOP/oop.py
@@ -1,26 +1,26 @@
 # Accessing and modifying data:
 # 1. The traditional way: make the data private and use 
 # getters and setters:
+# 2. Properties
 
 
-from datetime import datetime
 class User:
     def __init__(self, username, email, password):
         self.username = username
         self._email = email
         self.password = password
 
-    def get_email(self):
-        print(f"Email accessed at {datetime.now()}")
+    @property
+    def email(self):
+        print("Email Accessed")
         return self._email
 
-    def set_email(self, new_email):
+    @email.setter
+    def email(self, new_email):
         if "@" in new_email:
             self._email = new_email
 
+
 user1 = User("datheman", "Dan@gmail.com", "123")
-print(user1.get_email())
-
-user1.set_email("danny@gmail.com")
-print(user1.get_email())
-
+user1.email = "this@gmail.com"
+print(user1.email)
