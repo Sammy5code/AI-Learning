117326828570f98acc4953502b474f06b69a5f93
Sammy5code <najeemgbolahan4@gmail.com>
2026-09-08 15:34:02 +0100

    Getters

--- Python OOP/oop.py
+++ Python OOP/oop.py
@@ -1,14 +1,26 @@
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age 
- 
-    def greet(self):
-        print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+# Accessing and modifying data:
+# 1. The traditional way: make the data private and use 
+# getters and setters:
 
-person1 = Person("Alice", 30)
-person1.greet()
 
-person2 = Person("Bob", 42)
-person2.greet()
+from datetime import datetime
+class User:
+    def __init__(self, username, email, password):
+        self.username = username
+        self._email = email
+        self.password = password
+
+    def get_email(self):
+        print(f"Email accessed at {datetime.now()}")
+        return self._email
+
+    def set_email(self, new_email):
+        if "@" in new_email:
+            self._email = new_email
+
+user1 = User("datheman", "Dan@gmail.com", "123")
+print(user1.get_email())
+
+user1.set_email("danny@gmail.com")
+print(user1.get_email())
 
