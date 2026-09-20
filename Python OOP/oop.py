22420c22a86537013029f9941ea486d3b911500f
Sammy5code <najeemgbolahan4@gmail.com>
2026-09-20 15:58:08 +0100

    Polymorphism

--- Python OOP/oop.py
+++ Python OOP/oop.py
@@ -1,35 +1,67 @@
-# Inheritance
+# Polymorphism
 
 class Vehicle:
+     def __init__(self, brand, model, year):
+          self.brand = brand
+          self. model = model
+          self.year = year
+
+     def start(self):
+          print("Vehicle is starting.")
+     def stop(self):
+          print("Vehicle is stopping")
+
+          
+class Car(Vehicle):
+    def __init__(self, brand, model, year, number_of_doors):
+        super().__init__(brand, model, year)
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.number_of_doors = number_of_doors
+
+    def start(self):
+        print("Car is starting")
+
+    def stop(self):
+        print("Car is stopping")
+
+class Motorcycle(Vehicle):
     def __init__(self, brand, model, year):
+        super().__init__(brand, model, year)
         self.brand = brand
         self.model = model
         self.year = year
 
     def start(self):
-        print("Vehicle is starting")
+        print("Motorcycle is starting.")
 
     def stop(self):
-        print("Vehicle is stopping")
+        print("Motorcycle is stopping.")
 
 
-class Car(Vehicle):
-    def __init__(self, brand, model, year, number_of_doors, number_of_wheels):
-        super().__init__(brand, model, year)
-        self.number_of_doors = number_of_doors
-        self.number_of_wheels = number_of_wheels
+vehicles = [
+    Car("Ford", "Focus", 2008, 5),
+    Motorcycle("Honda", "Scoopy", 2008)
+]
 
+for vehicle in vehicles:
+     if isinstance(vehicle, Vehicle):
+          print(f"inspecting {vehicle.brand} {vehicle.model} ({type(vehicle).__name__})")
+          vehicle.start()
+          vehicle.stop()
+          
+# Loop through list of vehicles and inspect them
+# for vehicle in vehicles:
+#     if isinstance(vehicle, Car):
+#         print(f"inspecting {vehicle.brand} {vehicle.model} ({type(vehicle).__name__})")
+#         vehicle.start()
+#         vehicle.stop()
+#     elif isinstance(vehicle, Motorcycle):
+#                 print(f"inspecting {vehicle.brand} {vehicle.model} ({type(vehicle).__name__})")
+#                 vehicle.start_bike()
+#                 vehicle.stop_bike
 
-class Bike(Vehicle):
-    def __init__(self, brand, model, year, number_of_wheels):
-        super().__init__(brand, model, year)
-        self.number_of_wheels = number_of_wheels
+#     else:
+#          raise Exception("Obect is not a valid vehicle")
 
-
-car = Car("ford", "Focus", 2008, 5, 4)
-bike = Bike("Honda", "Scoopy", 2018, 2)
-print(car.__dict__)
-print(bike.__dict__)
-car.start()
-bike.start()
-        
