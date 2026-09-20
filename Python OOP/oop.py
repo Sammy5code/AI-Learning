5f41fd5df438df74ac8e318141a5b9e6179f6353
Sammy5code <najeemgbolahan4@gmail.com>
2026-09-20 14:31:11 +0100

    Static method

--- Python OOP/oop.py
+++ Python OOP/oop.py
@@ -1,20 +1,33 @@
-# Static Attributes
+# Static vs Instance Method Example
 
-class User:
-    user_count = 0
+class BankAccount:
+    MIN_BALANCE = 100
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-        User.user_count += 1
+    def __init__(self, owner, balance = 0):
+        self.owner = owner
+        self._balance = balance
 
-    def display_user(self):
-        print(f"Username: {self.username}. Email: {self.email}")
+    def deposit(self, amount):
+        if self._is_valid_amount(amount):
+            self._balance += amount
+            self._log_transaction("deposit", amount)
+        else:
+            print("Deposit amount must be positive.")
 
-user1 = User("Gbolahan", "gbo.gmail.com")
-user2 = User("Samuel", "sam@gmail.com")
+    def _is_valid_amount(self, amount):
+        return amount > 0 
 
-print(User.user_count)
-print(user1.user_count)
-print(user2.user_count)
+    def _log_transaction(self, transaction_type, amount):
+        print(f"Logging {transaction_type} of ${amount}. New Balance: ${self._balance}")
 
+    @staticmethod
+    def is_valid_interest_rate(rate):
+        return 0 <= rate <= 5 
+
+
+account = BankAccount("Alice", 500)
+account.deposit(200)
+
+
+        
+        
